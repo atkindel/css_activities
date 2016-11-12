2ef9abccfdb0673825847531927abb1c4a1e1bcd
Alex Kindel <alextkindel@gmail.com>
2016-11-12 17:16:30 -0500

    Adjust to new problem 2.7

--- 2.7/preprocess_ng2.sh
+++ 2.7/preprocess_ng2.sh
@@ -7,4 +7,12 @@
 mkdir -p data
 cd data
 curl http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-1gram-20120701-1.gz > ng2012.gz
-gunzip ng2012.gz > ng2012.tsv
+gunzip ng2012.gz
+mv ng2012 ng2012.tsv
+
+# Retrieve total counts
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-totalcounts-20120701.txt > tc.csv
+cat tc.csv | tr "\\t" "\\n" | tail -n +2 > tcn.csv
+echo -e "year,match_count,page_count,volume_count" | cat - tcn.csv > total_counts.csv
+rm tc.csv
+rm tcn.csv
