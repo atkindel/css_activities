66b4724e3b4a403b32588f9d4e38214a2a9ca8ec
Alex Kindel <alextkindel@gmail.com>
2016-11-14 21:24:34 -0500

    Update solutions to 2.7

--- 2.7/find_years.sh
+++ 2.7/find_years.sh
@@ -5,13 +5,13 @@ cd data
 rm -rf years
 mkdir -p years
 
-# awk script to find ngrams matching year
+# awk script to find ngrams matching specific year
 function find_year {
     echo -e "ngram\tyear\tmatch_count\tvolume_count" > years/$1.tsv
     awk -F\t -v year=$1 '$1 == year' ng2012.tsv >> years/$1.tsv
+    grep '\d{4}\,.*' ng2012.tsv >> testout.tsv
 }
 
-# Find years of interest
-find_year 1883
-find_year 1910
-find_year 1950
+# Find all year ngrams from 1700 onward
+echo -e "ngram\tyear\tmatch_count\tvolume_count" > ngyears.tsv
+grep '^[1-2][7-9][0-9][0-9]\t.*' ng2012.tsv >> ngyears.tsv
